91f9e43798503a931877615bb957649728ff206b
rockband_666 <rockband_666@hotmail.com>
2024-01-23 22:14:50 -0400

    put para editar parametros

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 
@@ -9,7 +9,7 @@ from fastapi.responses import HTMLResponse
 
 
 #esto es para cambiar el nombre de lo que sale en la pantalla de docs
-
+#parametro por ruta 
 app = FastAPI()
 app.title = "Mi aplicacion con FastAPI" #para cambiar el nombre
 app.version = "0.0.1" #para cambiar la version 
@@ -18,18 +18,64 @@ movies = [
     {
         "id": 1,
         "title": "Avatar",
-        "override": "es un exuberante planeta llamado Pandora viven los Na'vi",
+        "overview": "es un exuberante planeta llamado Pandora viven los Na'vi",
+        "year": "2019",
+        "rating": 7.8,
+        "category": "Accion"
+    },
+    {
+        "id": 2,
+        "title": "Avatar",
+        "overview": "es un exuberante planeta llamado Pandora viven los Na'vi",
         "year": "2019",
         "rating": 7.8,
         "category": "Accion"
     }
 ]
 
-@app.get('/', tags=['home'])
+@app.get('/', tags=['home']) #pertenece a home
 def message():
     #return "Hello, world!"
     return HTMLResponse('<h1> Hello, world!</h1>')
 
-@app.get('/movies/', tags=['movies'])
+@app.get('/movies', tags=['movies'])
 def get_movies():
     return movies
+
+#con parametro por ruta
+@app.get('/movies/{id}', tags=['movies'])
+def get_movies_(id: int):
+    #para el filtrado de las peliculas 
+    for item in movies:
+        if item["id"] == id:
+            return item
+    return []
+
+#filtrado de peliculas por su categoria 
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category:str, year:int):
+    #return [item for item in  movies if item['category'] == category ]
+    return category
+
+@ app.post('/movies/', tags=['movies'])
+def create_movies(id:int = Body(), title: str = Body(), overview:str = Body(), year:int = Body(), rating:str = Body(), category:str = Body()):
+    movies.append({
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
+    return movies
+#esto es para actualizar o editar datos 
+@app.put('/movies/{id}', tags=['movies'])
+def update_movies(id:int, title: str = Body(), overview:str = Body(), year:int = Body(), rating:str = Body(), category:str = Body()):
+    for item in movies:
+        if item['id'] == id:
+            item['title'] = title,
+            item['overview'] = overview,
+            item['year'] = year,
+            item['rating'] = rating,
+            item['category'] = category
+            return movies
