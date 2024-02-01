a871dadabc427f948c870007f002c3831775a06f
rockband_666 <rockband_666@hotmail.com>
2024-02-01 10:18:34 -0400

    como validar y crear token mediante JWT

--- main.py
+++ main.py
@@ -71,10 +71,13 @@ movies = [
 def message():
     #return "Hello, world!"
     return HTMLResponse('<h1> Hello, world!</h1>')
+
 #crear una ruta para que pueda loguearse el usuario
 @app.post('/login', tags=['auth'])
 def login(user: User):
-    return user
+    if user.email =="admin@gmail.com" and user.password == "admin":
+        token: str =  create_token(user.model_dump())
+        return JSONResponse(status_code=200, content={"token": token})
 
 
 
